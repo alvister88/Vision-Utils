cfd234b80eed344074fd471a02364755824c0701
alvister88 <ayzhu2022@gmail.com>
2024-04-23 22:43:48 -0700

    Created init files for packages; making repo into pip installable

---
+++ visionml-utils/__init__.py
@@ -0,0 +1,4 @@
+from .YOLO.yolov8.scripts.ModelTrainer import ModelTrainer
+from .YOLO.yolov8.scripts.ModelTuner import ModelTuner
+from .YOLO.yolov8.scripts.ModelValidator import ModelValidator
+from .YOLO.yolov8.scripts.VideoProcessor import VideoProcessor
---
+++ visionml-utils/yolov8/scripts/__init__.py
@@ -0,0 +1,4 @@
+from .ModelTrainer import ModelTrainer
+from .VideoProcessor import VideoProcessor
+from .ModelTuner import ModelTuner
+from .ModelValidator import ModelValidator
